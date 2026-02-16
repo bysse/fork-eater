b4e16c4eb19b608e73fc6a80cae5a217bc427a25
Erik Byström <erik.bystrom@gmail.com>
2026-02-16 21:01:40 +0100

    Added softshadows

--- libs/raymarching_vec3.glsl
+++ libs/raymarching_vec3.glsl
@@ -3,6 +3,7 @@
 #pragma slider(RAYMARCH_MAX_DISTANCE, 5, 1000, "Max Dist")
 #pragma switch(RAYMARCH_RELAXED, false, "Normal tracing", "Relaxed tracing")
 #pragma switch(RAYMARCH_INTERNAL, false, "March positive", "March both")
+#pragma switch(RAYMARCH_SOFT_SHADE_SIMPLE, false, "Simple softshade", "Penumbra softshade")
 #pragma endgroup()
 
 #ifndef EPS
@@ -100,3 +101,35 @@ vec4 intersect(vec3 ro, vec3 rd) {
 #endif
     return hit;
 }
+
+#ifdef RAYMARCH_SOFT_SHADE_SIMPLE
+float softshadow( in vec3 ro, in vec3 rd, float mint, float maxt, float k )
+{
+    float h, res = 1.0;
+    float t = mint;
+    for( int i=0; i<RAYMARCH_STEPS && t<maxt; i++ )
+    {
+        h = map(ro + rd*t).x;
+        if( h<mint )
+            return 0.0;
+        res = min( res, k*h/t );
+        t += h;
+    }
+    return res;
+}
+#else
+float softshadow(vec3 ro, vec3 rd, float mint, float maxt, float w )
+{
+    float res = 1.0;
+    float t = mint;
+    for( int i=0; i<RAYMARCH_STEPS && t<maxt; i++ )
+    {
+        float h = map(ro + t*rd).x;
+        res = min( res, h/(w*t) );
+        t += clamp(h, 0.005, 0.50);
+        if( res<-1.0 || t>maxt ) break;
+    }
+    res = max(res,-1.0);
+    return 0.25*(1.0+res)*(1.0+res)*(2.0-res);
+}
+#endif
